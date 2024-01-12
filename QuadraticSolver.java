7b3267c196040012432d8622b555f4e2fec43cb6
Dominic Rosato <drosato@polytechnic.org>
2024-01-12 14:43:19 -0800

    lets go

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,6 +1,6 @@
 public class QuadraticSolver{
     public static void main(String[] args) {
-        System.out.println("Welcome to the Quadratic solver!");
+        System.out.println("Let's goooo quadratic equations!");
         System.out.println("The quadratic formula is (-b +- sqrt(b^2-4ac))/2a");
     }
 }
